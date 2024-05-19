6dfa7897b10fede274ebb9f17a6f64c2841b5e5c
Sagar <sagarpuniyani@gmail.com>
2024-05-19 20:55:33 +0530

    chore: Add @radix-ui/react-toast and axios dependencies

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,22 +1,56 @@
-'use client';
-import { useSession, signIn, signOut } from "next-auth/react"
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+import { signUpValidation } from "@/Schemas/signUpSchemas"
+import axios from "axios";
 
-export default function Component() {
-    
-const { data: session } = useSession()
 
-if (session) {
+
+const Page = () => {
+
+    const [username , setUsername ] = useState('');
+    const [usernameMessage , setUsernameMessage ] = useState('');
+    const [isCheckingUsername , setisCheckingUsername] = useState(false);
+    const [isSubmitting  , setisSubmitting ] = useState(false);
+    const DobouncedUsername  = useDebounceValue(username, 500);
+    const {toast}  = useToast();
+    const router = useRouter();
+
+    // zod implementation 
+    const form = useForm <z.infer<typeof signUpValidation>>({
+        resolver : zodResolver(signUpValidation),
+        defaultValues: {
+            username: '',
+            email: '',
+            password: '',
+        },
+    })
+
+    useEffect(()=> {
+        const checkUniqueUsername = async () => {
+            if (DobouncedUsername){
+                setisCheckingUsername(isCheckingUsername => !isCheckingUsername);
+                setUsernameMessage('');
+                try {
+                    const response = await axios.get(`/api/unique-username?username=${DobouncedUsername}`)
+                } catch (error) {
+                    
+                }
+            }
+        }
+    } , [])
+
     return (
-    <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-    </>
+        <div>
+        
+        </div>
     )
     }
-    return (
-        <>
-        Not signed in <br />
-        <button className="bg-orange-400 px-2 py-1 rounded m-3" onClick={() => signIn()}>Sign in</button>
-        </>
-    )
-}
+
+export default Page;
